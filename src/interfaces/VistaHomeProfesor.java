a9d5b4c3910d7aa9da8e9f7d9469b2a1c84de2e9
cmarti0747 <carlosmartiglez@hotmail.com>
2017-05-02 11:35:30 +0200

    asc

--- src/interfaces/VistaHomeProfesor.java
+++ src/interfaces/VistaHomeProfesor.java
@@ -1,24 +1,28 @@
 package interfaces;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.SpringLayout;
 
 public class VistaHomeProfesor {
-	
+	private SpringLayout layout;
 	private JFrame ventana;
 	private Container contenedor;
 	private JButton desconectar;
 	private JLabel nombre,icono,academia;
 	private JComboBox<String> asignaturas,alumnos;
 	JTable matriculas;
-	private JPanel HomeField;
+	private JPanel HomeField,ExitField;
 	ControladorVistaHomeProfesor controlador;
 
 	public VistaHomeProfesor() {
@@ -27,13 +31,36 @@ public class VistaHomeProfesor {
 		// obtener contenedor, asignar layout
 		contenedor = ventana.getContentPane();
 		contenedor.setLayout(new BorderLayout());
+		desconectar = new JButton("Desconectar");
 		
 		HomeField = new JPanel();
+		layout = new SpringLayout();
+		HomeField.setLayout(layout);
 		asignaturas = new JComboBox<String>();
-		asignaturas.addItem("Padsof");
+		alumnos = new JComboBox<String>();
+		layout.putConstraint("HorizontalCenter", asignaturas, 200, "HorizontalCenter",HomeField);
+		layout.putConstraint("VerticalCenter", asignaturas, 0, "VerticalCenter", HomeField);
 		HomeField.add(asignaturas);
+		HomeField.add(alumnos);
+		HomeField.add(asignaturas);
+		HomeField.setVisible(true);
+		HomeField.setBackground(new Color(0,255,255));
 		
-		desconectar = new JButton("Desconectar");
+		
+		ExitField = new JPanel();
+		ExitField.setLayout(new BorderLayout());
+		icono = new JLabel();
+		academia= new JLabel("Academia Lopez");
+		academia.setFont(new Font("Arial",Font.PLAIN,90));
+		icono.setIcon(new ImageIcon("src/imagenes/descarga.png"));
+		ExitField.setBackground(new Color(255,255,255));
+		ExitField.add(icono,BorderLayout.WEST);
+		ExitField.add(academia,BorderLayout.CENTER);
+		ExitField.add(desconectar,BorderLayout.EAST);
+		
+		
+		contenedor.add(ExitField,BorderLayout.NORTH);
+		
 		contenedor.add(HomeField,BorderLayout.CENTER);
 		
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
